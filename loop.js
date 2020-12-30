552a7fc30c8a6f20a24d34a89b38fc474d3883eb
milkywoosh <archer.lukman@gmail.com>
2020-12-31 00:58:14 +0700

    add codecademyHTML for exercise

--- loop.js
+++ loop.js
@@ -13,25 +13,25 @@
 // } while (i <= 5);
 
 
-var input = "Bagong pergi mencari jus jeruk"
+// var input = "Bagong pergi mencari jus jeruk"
 
-if (input) {
-  let toDisplay = ' ';
-  for (let i=0; i<=input.length; i++) {
+// if (input) {
+//   let toDisplay = ' ';
+//   for (let i=0; i<=input.length; i++) {
     
-    if (input[i] === '') {
-      console.log(toDisplay);
-      toDisplay = '';
-  } else {
-    toDisplay+=input[i];
-  }
+//     if (input[i] === '') {
+//       console.log(toDisplay);
+//       toDisplay = '';
+//   } else {
+//     toDisplay+=input[i];
+//   }
 
-    if (i === (input.length)-1) {
-      console.log(toDisplay)
-    }
+//     if (i === (input.length)-1) {
+//       console.log(toDisplay)
+//     }
   
-  }
-}
+//   }
+// }
 
 
 /**
@@ -58,3 +58,64 @@ if (input) {
 //   }
 // }
 // console.log(output);
+
+
+// var times = 5;
+// while (times >= 0) {
+//   console.log(times, "meow");
+//   times --;
+// }
+
+
+
+// let a = 80;
+// let count = 0;
+// while (count <= 10) {
+//     // count;
+//     if (a%count  == 0) {
+//       console.log(a%count);
+//     } else {
+//     console.log(count, a%count, 'no zero');
+//   }
+//   count++;
+// }
+
+
+/**
+ * Buatlah sebuah program yang akan mencari sebuah string di dalam string.
+ * String input akan terdapat pada variable input.
+ * String yang dicari akan terdapat pada variable search.
+ *
+ * Output yang diharapkan dari program adalah:
+ *   - true  : jika search terdapat dalam input
+ *   - false : jika search tidak terdapat dalam input
+ */
+
+// var input = "Fisika SMA Terpadu Kelas XI";
+// var search = "SMA";
+// var inputIter='';
+// var searchIter='';
+
+// for (let i=0; i<search.length; i++) {
+//   for (let j=0; j<input.length; j++) {
+    
+//     if (input[j] === ' ') {
+//       console.log(inputIter);
+      
+//       inputIter;
+//     } else if (search[i] != 'SMA'){
+//       console.log(searchIter);
+//       searchIter;
+      
+//       // console.log('nothing');
+//     } else {
+//       searchIter+=search[i];
+//       inputIter+=input[j];
+//       // console.log('last part');
+//     }
+//   }
+// }
+
+var cek = "the things inside";
+var n = cek.includes("things");
+console.log(n)
